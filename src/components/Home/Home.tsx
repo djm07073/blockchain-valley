14e47a6db1bedeac599f7b500da23c61704cb4e0
suha jin <89185836+djm07073@users.noreply.github.com>
2023-09-07 00:40:29 +0900

    fix: correct comment

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
+      <h1 className="text-4xl font-bold mb-4">Welcome to Blockchain Valley</h1>
       <h2 className="text-2xl">Home: Scheduled to Open in October 🎉</h2>
     </div>
   );
